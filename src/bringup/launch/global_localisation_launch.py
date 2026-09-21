3a52fc737f668aab1c101b6dc5fccad4c0443c34
jeremyacox <127087864+jeremyacox@users.noreply.github.com>
2026-09-21 10:39:38 +1000

    Robot localization ekf (#5)
    * initial commit
    
    * update launch and package files
    
    * data
    
    * add global ekf
    
    * config changes
    
    * mavros config covariance fix
    
    * small test
    
    * mocap test data
    
    * data analysis
    
    * ekf data collection
    
    * Add production wheel IMU and GPS EKF launch

---
+++ src/bringup/launch/global_localisation_launch.py
@@ -0,0 +1,120 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+LOCAL_EKF_CONFIG_FILE = os.path.join(
+        get_package_share_directory('localisation'),
+        'config',
+        'local_ekf_wheel_imu.yaml',
+    )
+
+GLOBAL_EKF_CONFIG_FILE = os.path.join(
+        get_package_share_directory('localisation'),
+        'config',
+        'global_ekf_local_gps.yaml',
+    )
+
+NAVSAT_CONFIG_FILE = os.path.join(
+        get_package_share_directory('bringup'),
+        'config',
+        'navsat_transform_node.yaml',
+    )
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    roboclaw_node = Node(
+        package='control',
+        executable='roboclaw_for_motors',
+        name='roboclaw_for_motors',
+        output='screen',
+    )
+
+    mavros_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('bringup'),
+                'launch',
+                'mavros-test.py',
+            )
+        )
+    )
+
+    
+    encoder_node = Node(
+        package='localisation',
+        executable='encoder_localisation',
+        name='encoder_localisation',
+        output='screen',
+        parameters=[{
+            'use_sim_time': use_sim_time,
+        }],
+    )
+
+    local_ekf_node = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_filter_node_local',
+        output='screen',
+        parameters=[
+            LOCAL_EKF_CONFIG_FILE,
+            {
+                'use_sim_time': use_sim_time,
+            },
+        ],
+        remappings=[
+            ('odometry/filtered', '/odometry/local'),
+        ],
+    )
+
+    global_ekf_node = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_filter_node_global',
+        output='screen',
+        parameters=[
+            GLOBAL_EKF_CONFIG_FILE,
+            {
+                'use_sim_time': use_sim_time,
+            },  
+        ],
+        remappings=[
+            ('odometry/filtered', '/odometry/global'),
+        ],
+    )
+
+    navsat_node = Node(
+        package='robot_localization',
+        executable='navsat_transform_node',
+        name='navsat_transform',
+        output='screen',
+        parameters=[
+            NAVSAT_CONFIG_FILE,
+            {'use_sim_time': use_sim_time},
+        ],
+        remappings=[
+            ('imu', '/mavros_fcu/mavros_fcu/data'),
+            ('gps/fix',
+            '/mavros_fcu/mavros_fcu/global_position/raw/fix'),
+            ('odometry/filtered', '/odometry/local'),
+            ('odometry/gps', '/odometry/gps'),
+        ],
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+        ),
+        roboclaw_node,
+        mavros_launch,
+        encoder_node,
+        local_ekf_node,
+        global_ekf_node,
+        navsat_node,
+    ])
